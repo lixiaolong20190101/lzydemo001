93256998dff2f10075e3a65e330b8741895e1ba9
李少龙 <2718893665@qq.com>
2024-07-02 09:02:39 +0800

    诚信为本

---
+++ src/main/java/com/example/lzydemo001/Lzydemo001Application.java
@@ -0,0 +1,13 @@
+package com.example.lzydemo001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lzydemo001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lzydemo001Application.class, args);
+    }
+
+}
